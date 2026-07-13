db10ee773906c6d7c390a311ec6d2a380a4ff4c0
miinorii <77248345+miinorii@users.noreply.github.com>
2026-07-13 17:39:44 +0200

    add Line as a CST for Vec<Token>

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod field_keys;
 pub mod lexer;
 pub mod settings;
 pub mod ssh_config;
+pub mod line;
---
+++ src/line.rs
@@ -0,0 +1,63 @@
+use std::{iter::Peekable, vec};
+use crate::lexer::{Token, TokenKind};
+
+pub enum Line {
+    Directive { indent: Option<Token>, key: Token, sep: Token, value: Token, ending: Option<Token>},
+    Comment { indent: Option<Token>, text: Token, ending: Option<Token>},
+    Blank { indent: Option<Token>, ending: Option<Token>},
+}
+
+impl Line {
+    /// Parse multiple `Line` from a `Vec<Token>`.
+    pub fn parse_lines(tokens: Vec<Token>) -> Result<Vec<Self>, String> {
+        let mut iter = tokens.into_iter().peekable();
+        let mut lines: Vec<Self> = Vec::new();
+        while iter.peek().is_some() {
+            lines.push(Self::parse_line(&mut iter)?);
+        }
+        return Ok(lines);
+    }
+
+    /// Parse the next line from the token stream.
+    /// 
+    /// Assume each line can either be one of the following pattern:
+    /// 
+    /// - `[indent], comment, [line_ending]`
+    /// - `[indent], key, separator, value, [line_ending]`
+    /// - `indent, [line_ending]`
+    /// - `line_ending`
+    /// 
+    /// Optionnal token are denoted with `[]`
+    fn parse_line(iter: &mut Peekable<vec::IntoIter<Token>>) -> Result<Line, String> {
+        let indent = iter.next_if(|t| t.kind == TokenKind::WhiteSpace);
+        match iter.peek().map(|t| &t.kind) {
+            Some(TokenKind::Comment) => Ok(Line::Comment { 
+                indent, 
+                text: iter.next().unwrap(),
+                ending: iter.next_if(|t| t.kind == TokenKind::LineEnding) 
+            }),
+            Some(TokenKind::LineEnding) | None => Ok(Line::Blank { 
+                indent, 
+                ending: iter.next() // None if EOF 
+            }),
+            Some(TokenKind::FieldKey) => {
+                let key = iter.next().unwrap();
+                let sep = iter
+                    .next_if(|t| t.kind == TokenKind::FieldSeparator)
+                    .ok_or("expected FieldSeparator")?;
+                let value = iter
+                    .next_if(|t| t.kind == TokenKind::FieldValue)
+                    .ok_or("expected FieldValue")?;
+                let ending = iter.next_if(|t| t.kind == TokenKind::LineEnding);
+                return Ok(Line::Directive { 
+                    indent, 
+                    key, 
+                    sep, 
+                    value, 
+                    ending 
+                })
+            },
+            Some(other) => Err(format!("unexpected token: {other:?}"))
+        }
+    }
+}
