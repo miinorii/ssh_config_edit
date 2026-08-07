3f6692f08704adba27721728b01200a9be8d8ea6
miinorii <77248345+miinorii@users.noreply.github.com>
2026-08-07 14:58:45 +0200

    lib doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+//! This crate provides high and low level abstractions for 
+//! reading and editing ssh_config files non-destructively.
+//! 
+//! [SSHConfig](crate::SSHConfig) `parse` -> `to_string()` is byte-identical, 
+//! editing key K only changes the bytes of K's line.
+//! 
+//! The semantic layer is accessible throught a lossy high-level abstraction with 
+//! [SSHConfig](crate::SSHConfig), [HostSettings](crate::HostSettings), 
+//! [Field](crate::Field) and [FieldKey](crate::FieldKey).
+//! 
+//! ```rust
+//! use ssh_config_edit::SSHConfig;
+//! 
+//! fn main() {
+//!     todo!()
+//! }
+//! 
+//! ```
+//! 
+//! 
+//! The document layer is accessible throught a lossless low-level abstraction with 
+//! [SSHConfig](crate::SSHConfig), [Section](crate::Section) and [Line](crate::Line).
+//! 
+//! ```rust
+//! use ssh_config_edit::SSHConfig;
+//! 
+//! fn main() {
+//!     todo!()
+//! }
+//! 
+//! ```
+
 pub mod field_keys;
 pub mod lexer;
 pub mod line;
